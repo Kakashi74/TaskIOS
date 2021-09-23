c40cabedf43c09f48b8003159345766bdd824661
Ahmed Mohamed <AhmedKaKashi74@gmail.com>
2021-09-23 15:45:55 +0200

    Initial Commit

---
+++ TaskIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskIOS
+//
+//  Created by Ahmed on 9/23/21.
+//  Copyright © 2021 eramint.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
